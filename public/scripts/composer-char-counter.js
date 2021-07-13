2067277fb2640de6e82851ceaabc32523aaf2013
Xiaowei <xiaowei.c.li@gmail.com>
2021-07-13 20:58:56 +0000

    add logic so that counter is red when more than 140 chars

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,21 +2,26 @@ $(document).ready(function() {
   console.log("within")
 
 
-  $("#tweet-text").on('input', function() {
-    //use function so that this properly refers to your element
-
-    let tweet = $(this).val();//assigns value of this to text count
-
-    //finds the difference between allowed chars and tweet length
-    let charDiff = 140 - tweet.length;
-
-    $('.counter').val(charDiff);//assigns val to charDiff
-
-    if (charDiff < 0) {//changes color if it's negative val
-      $(".counter").css("color", "red");
-    } else {
-      $(".counter").css("color", "#545149");
-    }
-  })
+  $("#tweet-text").on('input', onInput);
 
 });
+
+
+const onInput = function() {
+  //use function so that this properly refers to your element
+
+  let tweet = $(this).val();//assigns value of this to text count
+
+  //finds the difference between allowed chars and tweet length
+  let charDiff = 140 - tweet.length;
+  //finds the output tag from within textarea 
+  const output = $(this).next().find('output');
+  //assigns val to charDiff to keep track of chars
+  $('output').val(charDiff);
+
+  if (charDiff < 0) {//changes color if it's negative val
+    $("output").css("color", "red");
+  } else {
+    $("output").css("color", "#545149");
+  }
+}
