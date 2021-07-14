68f687173f028b2bb1022f7fa4fc418a92e6d5bf
Xiaowei <xiaowei.c.li@gmail.com>
2021-07-14 17:53:12 +0000

    implement createTweetELement function: parameter is an object, inside function extract important values from tweet object and assign to var. create html object and interpolate your html from index.html, and insert important vals as variables inside, create DOM, return it

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,5 +2,76 @@
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+*/
+
+//TESTER DATA
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+};
+
+$(document).ready(function() {
+  
+  const $tweet = createTweetElement(tweetData);
+  // Test / driver code (temporary)
+  console.log($tweet); // to see what it looks like
+  // $('#tweets-container').append($tweet); 
+  // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  $('#tweet-container').append($tweet);
+
+
+
+});
+
+
+//-------FUNCTIONS------------------------------------------------------------------------------
+
+const createTweetElement = function(tweetObject) {
+
+  const {name, avatars, handle} = tweetObject.user
+  const {text} = tweetObject.content
+  
+  const timeSinceCreation = timeago.format(tweetObject.created_at);
+  
+  const htmlMarkup = `
+    <article class="tweet">
+      <header class="user-info">
+        <div class="tweet-img-name">
+          <img src=${avatars} >
+          <span class="name">${name}</span>
+        </div>
+        <div class="tweet-handle">
+          <span>${handle}</span>
+        </div>
+      </header>
+      <p class="tweet-text">
+        ${text}
+      </p>
+      <footer>
+        <div class="tweet-footer">
+          <span>${timeSinceCreation}</span>
+          <div class="tweet-icons">
+            <i class="fas fa-flag"></i>
+            <i class="fas fa-retweet"></i>
+            <i class="fas fa-heart"></i>
+          </div>
+        </div>
+      </footer>
+    </article>
+  `;
+
+  const $tweet = $(htmlMarkup)
+  // document.body.innerHTML = htmlMarkup;
+  // console.log(document.body.innerHTML); //entire page becomes htmlMarkup
+
+  return $tweet;
+};
+
 
